ac6cdb47f984dd3283ce108a11ba4a2c3155fe8c
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-05-23 12:20:55 +0800

    Added parameters to config.py

--- config.py
+++ config.py
@@ -1,2 +1,18 @@
 # Defines fixed parameters
 # IP addresses, MAC addresses, routing tables
+
+# IP Addresses
+HOST_A_IP = "10.0.1.10"
+HOST_B_IP = "10.0.2.20"
+R1_INTERFACE_1_IP = "10.0.1.1"
+R1_INTERFACE_2_IP = "10.0.2.1"
+
+# MAC Addresses
+HOST_A_MAC = "AA:AA:AA:AA:AA:AA"
+HOST_B_MAC = "BB:BB:BB:BB:BB:BB"
+R1_INTERFACE_1_MAC = "CC:CC:CC:CC:CC:CC"
+R1_INTERFACE_2_MAC = "DD:DD:DD:DD:DD:DD"
+
+# Parameters
+MAX_SEGMENT_LIMIT = 500
+TTL = 100
