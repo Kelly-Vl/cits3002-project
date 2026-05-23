14ea175eadef76442453738041319ec091a824f8
Kelly-Vl <kelly.valencia2006@gmail.com>
2026-05-23 20:49:18 +0800

    Added comments to main.py

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ from config import *
 from devices import * 
 
 def main():
+    # Error handling: Incorrect input size
     if len(sys.argv) != 2:
         print("Usage: python main.py <message_size>")
         return
     
+    # Get the command-line arguments
     try:
         message_size = int(sys.argv[1])
+
+    # Error handling: Incorrect input type 
     except ValueError:
         print("Error: message_size must be an integer")
         return
     
+    # Error handling: Must send non-negative size
     if message_size < 0:
         print("Error: message_size must be 0 or greater")
         return
     
+    # Create the simulated Hosts (Host A and Host B)
     host_a = Host(
         name="Host A",
         ip=HOST_A_IP,
@@ -37,18 +43,22 @@ def main():
         arp_table=ARP_TABLE_HOST_B
     )
 
+    # Create the simulated Router R1
     router = Router(
         name="Router R1",
         routing_table=ROUTING_TABLE_R1,
         arp_table=ARP_TABLE_R1
     )
 
+    # Store devices globally (so they can access each other during simulation)
     NETWORK_DEVICES["Host A"] = host_a
     NETWORK_DEVICES["Host B"] = host_b
     NETWORK_DEVICES["Router R1"] = router
 
+    # Create data payload
     data = "X" * message_size
 
+    # Sending data from Host A to Host B
     host_a.send_data(
         data=data,
         dst_ip=HOST_B_IP,
